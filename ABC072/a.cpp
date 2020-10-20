a65f972f947de4694aec5446be6823a30c36bf3c
ryongyong <67895239+ryongyong@users.noreply.github.com>
2020-10-20 23:39:00 +0900

    Add files via upload

---
+++ ABC072/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+    int X , t ; 
+    cin >> X >> t ; 
+    int ans = X - t ; 
+    if(ans < 0 ) ans = 0 ; 
+    
+    cout << ans << endl;
+}
