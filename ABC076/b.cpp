f4ce86c99c3666e85c4f3662747788a602b1f18b
ryongyong <67895239+ryongyong@users.noreply.github.com>
2020-10-23 23:14:25 +0900

    Add files via upload

---
+++ ABC076/b.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+int main(){
+  int N , K ;
+  cin >> N >> K ; 
+  int ans = 1 ;
+  for(int i = 0 ; i < N ; i++){
+    if(ans < K){
+      ans *= 2 ; 
+    }
+    else{
+      ans += K ; 
+    }
+  }
+  
+  cout << ans << endl ;
+}
